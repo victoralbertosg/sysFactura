8e6e229400b0580ad7bcb421398bfa62b7488253
Vasg <Vasg@LAPTOP-2J14PTSC>
2019-04-26 23:39:29 -0500

    subirImagen

--- src/main/java/com/titannet/springboot/app/controller/ClienteController.java
+++ src/main/java/com/titannet/springboot/app/controller/ClienteController.java
@@ -1,7 +1,11 @@
 package com.titannet.springboot.app.controller;
 
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
+
 
 import javax.validation.Valid;
 
@@ -9,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.titannet.springboot.app.entity.Cliente;
 import com.titannet.springboot.app.service.IClienteService;
@@ -46,6 +52,13 @@ public class ClienteController {
 		model.addAttribute("page", pageRender);
 		return "listar";
 	}
+	@RequestMapping(value = "/ver/{id}", method = RequestMethod.GET)
+	public String ver (@PathVariable(value="id") Long id, Model model){
+		Cliente cliente =clienteService.findOne(id);
+		model.addAttribute("titulo", "ver clientes");
+		model.addAttribute("cliente", cliente);		
+		return "ver";
+	}
 
 
 	@RequestMapping(value = "/form", method = RequestMethod.GET)
@@ -57,10 +70,25 @@ public class ClienteController {
 	}
 
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, @RequestParam("file")MultipartFile foto,RedirectAttributes flash, SessionStatus status) {
 		if (result.hasErrors()) {
 			model.addAttribute("titulo","Formulario Cliente");
 			return "form";
+		}
+		if (!foto.isEmpty()) {
+			
+			Path directorioRecursos=Paths.get("src//main//resources//static/uploads");
+			String rootPath=directorioRecursos.toFile().getAbsolutePath();
+			try {
+				byte[] bytes=foto.getBytes();
+				Path rutaCompleta=Paths.get(rootPath +"//"+foto.getOriginalFilename());
+				Files.write(rutaCompleta, bytes);
+				flash.addFlashAttribute("info","has subido correctamente '" + foto.getOriginalFilename()+"'");
+				cliente.setFoto(foto.getOriginalFilename());
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
 		}
 		String mensajeFlash=(cliente.getId()!=null)? "Cliente Editado con éxito":"Cliente creado con éxito";
 		clienteService.save(cliente);
