58aae814e980b8189dc4d9f137fe57b1d6011c89
Vasg <Vasg@LAPTOP-2J14PTSC>
2019-05-23 23:04:48 -0500

    bddCulminado

--- spring-boot-data-jpa/src/main/java/com/titannet/springboot/app/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/titannet/springboot/app/entity/Cliente.java
@@ -1,14 +1,18 @@
 package com.titannet.springboot.app.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -19,10 +23,10 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
-@Table(name= "clientes")
+@Table(name = "clientes")
 public class Cliente implements Serializable {
 
-//	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,85 +48,77 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 	
-	private String foto;
-
-		public Long getId() {
-		return id;
+	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Factura> facturas;
+	
+	public Cliente() {
+		facturas = new ArrayList<Factura>();
 	}
 
-
+	private String foto;
+	
+	public Long getId() {
+		return id;
+	}
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
-
 	public String getNombre() {
 		return nombre;
 	}
-
-
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-
-
 	public String getApellido() {
 		return apellido;
 	}
-
-
 
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
 
-
-
 	public String getEmail() {
 		return email;
 	}
-
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
-
 	public Date getCreateAt() {
 		return createAt;
 	}
-
-
 
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
 
-	@PrePersist	
-	public void prePersist() {
-		createAt=new Date();
-	}
-	
-	
-
 	public String getFoto() {
 		return foto;
 	}
-
-
 
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
 
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
 
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+	
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
+	}
 
-	private static final long serialVersionUID = 1L;
-
+	@Override
+	public String toString() {
+		return nombre + " " + apellido;
+	}
 }
