fdd98a6b200028978060a5a924057fa20d2877d3
Vasg <Vasg@LAPTOP-2J14PTSC>
2019-05-31 00:06:09 -0500

    getFactura

--- spring-boot-data-jpa/src/main/java/com/titannet/springboot/app/controller/FacturaController.java
+++ spring-boot-data-jpa/src/main/java/com/titannet/springboot/app/controller/FacturaController.java
@@ -1,5 +1,39 @@
 package com.titannet.springboot.app.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.titannet.springboot.app.entity.Cliente;
+import com.titannet.springboot.app.entity.Factura;
+import com.titannet.springboot.app.service.IClienteService;
+
+@Controller
+@RequestMapping("/factura")
+@SessionAttributes("factura")
 public class FacturaController {
 
+	@Autowired
+	private IClienteService clienteService;
+
+	@GetMapping("/form/{clienteId}")
+	public String crear(@PathVariable(value = "clienteId") Long clienteId, Map<String, Object> model,
+			RedirectAttributes flash) {
+		Cliente cliente = clienteService.findOne(clienteId);
+		if (cliente == null) {
+			flash.addFlashAttribute("error", "el cliente no existe");
+			return "redirect:/listar";
+		}
+		Factura factura = new Factura();
+		factura.setCliente(cliente);
+		model.put("factura", factura);
+		model.put("titulo", "Crear Factura");
+		return "factura/form";
+	}
 }
